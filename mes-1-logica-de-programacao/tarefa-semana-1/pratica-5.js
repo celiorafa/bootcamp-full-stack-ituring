5207a3dfcf029c1341598b6282685279fee2ea70
Celio Rafael <cr2faria@gmail.com>
2022-10-08 18:16:38 -0300

    utilizar template string

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,7 @@
+let nome = "Celio Rafael"
+let email = "cr2faria@gmail.com"
+let job = "Programador"
+let age = 35
+let dream = "conhecer a Grécia"
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${dream}. Confirma pra gente alguns dados? A sua idade é ${age}, você é ${job} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
