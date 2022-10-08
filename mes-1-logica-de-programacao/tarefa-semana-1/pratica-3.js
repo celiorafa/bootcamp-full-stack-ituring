bda8ea9dbb34717ab354782597f211bfeeef0364
Celio Rafael <cr2faria@gmail.com>
2022-10-08 18:05:21 -0300

    comparar variaveis

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,11 @@
+let nome = "Celio Rafael"
+let email = "cr2faria@gmail.com"
+let job = "Programador"
+let age = 35
+let dream = "conhecer a Grécia"
+
+console.log(typeof(nome)=="string")
+console.log(typeof(email)=="string")
+console.log(typeof(job)=="string")
+console.log(typeof(age)=="string")
+console.log(typeof(dream)=="string")
