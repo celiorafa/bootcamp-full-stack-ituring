ea151c3deb673f710d9dc2291ee4608fde543d13
Celio Rafael <cr2faria@gmail.com>
2022-10-08 17:48:43 -0300

    inserir comando console.log

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -1,5 +1,11 @@
-let name = "Celio Rafael"
+let nome = "Celio Rafael"
 let email = "cr2faria@gmail.com"
 let job = "Programador"
 let age = 35
 let dream = "conhecer a Grécia"
+
+console.log(typeof(nome))
+console.log(typeof(email))
+console.log(typeof(job))
+console.log(typeof(age))
+console.log(typeof(dream))
