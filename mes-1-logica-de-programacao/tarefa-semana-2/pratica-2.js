5eb6a06ba1ec6df3bebb7a990cfb4f0d840c3136
Celio Rafael <cr2faria@gmail.com>
2022-10-14 11:22:02 -0300

    realizar pratica 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -0,0 +1,25 @@
+let nomeDoComprador
+let nomeDoProduto
+let nomeDeQuemRecebeu
+let enderecoDeEntrega
+let nomeDeQuemEnviouEmail
+
+function compra(nomeDoComprador) {
+    console.log(`Olá, ${nomeDoComprador}!`)
+}
+
+function produto(nomeDoProduto, nomeDeQuemRecebeu, enderecoDeEntrega){
+    console.log(`Fizemos a entrega do produto ${nomeDoProduto} no endereço ${enderecoDeEntrega} com sucesso!
+O seu produto foi recebido por: ${nomeDeQuemRecebeu}
+Esperamos que você esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que possamos ajudar.
+        `)
+
+}
+
+function confirmacao(nomeDeQuemEnviouEmail) {
+    console.log(`Atenciosamente, \n ${nomeDeQuemEnviouEmail}`)
+}
+
+compra("Antonio")
+produto("Televisão LG", "Maria", "Rua da Casa, 01")
+confirmacao("Thales")
