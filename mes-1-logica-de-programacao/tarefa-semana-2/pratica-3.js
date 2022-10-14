bd025b1a078da6136782d6b0755ea8e87520543b
Celio Rafael <cr2faria@gmail.com>
2022-10-14 11:51:23 -0300

    realizar pratica 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-3.js
@@ -0,0 +1,9 @@
+function numeroAleatorio(){
+    const min = 0
+    const max = 10000
+    const numero = Math.random()*((max-min)+min)
+
+    return parseInt(numero)
+}
+
+console.log(numeroAleatorio())
