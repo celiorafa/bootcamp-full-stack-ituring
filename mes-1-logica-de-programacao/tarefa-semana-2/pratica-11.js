3a273a357478fb5f768e7c85542eda9c258285df
Celio Rafael <cr2faria@gmail.com>
2022-10-15 16:05:20 -0300

    escolhendo cores

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-11.js
@@ -0,0 +1,34 @@
+const color =""
+
+switch (color) {
+        case "vermelha":
+        console.log ("rgb(255,0,0)")
+        break;
+
+        case "laranja":
+        console.log ("rgb(255,128,0)")
+        break;
+
+        case  "amarela":
+        console.log ("rgb(255,255,0)")
+        break;
+
+        case "verde":
+        console.log ("rgb(0,255,0)")
+        break;
+
+        case "azul claro":
+        console.log ("rgb(0,255,255)")
+        break;
+
+        case "azul marinho":
+        console.log ("rgb(0,0,255)")
+        break;
+
+        case "rosa":
+        console.log ("rgb(255,0,255)")
+        break;
+
+    default: console.log ("rgb(255,255,255)")
+        break;
+}
