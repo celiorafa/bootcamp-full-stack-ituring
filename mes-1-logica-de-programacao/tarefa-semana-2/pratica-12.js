e9424b8c43f4aa680658e8e84beaac9ca9cd4a18
Celio Rafael <cr2faria@gmail.com>
2022-10-15 16:11:44 -0300

    escolher dia da semana

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-12.js
@@ -0,0 +1,34 @@
+const diaSemana = 9
+
+switch (diaSemana) {
+        case 1:
+        console.log ("Segunda")
+        break;
+
+        case 2:
+        console.log ("Terça")
+        break;
+
+        case 3:
+        console.log ("Quarta")
+        break;
+
+        case 4:
+        console.log ("Quinta")
+        break;
+
+        case 5:
+        console.log ("Sexta")
+        break;
+
+        case 6:
+        console.log ("Sábado")
+        break;
+
+        case 7:
+        console.log ("Domingo")
+        break;
+
+    default: console.log (`O número ${diaSemana} não é valido para dia da semana!`)
+        break;
+}
