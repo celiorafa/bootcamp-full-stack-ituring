9df15e70bdad26ec38e36fb75e8ee60215e338c9
Celio Rafael <cr2faria@gmail.com>
2022-10-15 15:29:30 -0300

    programar tradutor

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-7.js
@@ -0,0 +1,18 @@
+function tradutor(idioma) {
+    if (idioma=="es") {
+        console.log("¡Hola mundo!")
+    } else {
+        if (idioma=="en") {
+            console.log("Hello World!")
+        } else {
+            if (idioma=="pt"){
+                console.log("Olá Mundo!")
+            } else {
+                console.log("Olá, Mundo!!!")
+            }
+        }
+    }
+
+}
+
+tradutor()
