afb1712753bf578ecb98233cc26d00344338008a
Celio Rafael <cr2faria@gmail.com>
2022-10-15 15:36:54 -0300

    verificar paridade

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-8.js
@@ -0,0 +1,11 @@
+
+function parOuImpar(numeroEscolhido) {
+    const verificacao = numeroEscolhido % 2;
+
+    if (verificacao)
+        console.log(`O número ${numeroEscolhido} é impar.`)
+    else
+        console.log(`O número ${numeroEscolhido} é par.`)
+}
+
+parOuImpar(10)
