b460e36e6a6db0d30e48742eff4af7b141e2a20f
Celio Rafael <cr2faria@gmail.com>
2022-10-19 21:07:54 -0300

    exibir numero impar

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-2.js
@@ -0,0 +1,15 @@
+function exibirImpares(n){
+    if(n>0){
+        i=1
+        while(i<=n){
+            console.log(i)
+            i=i+2
+        }
+
+
+
+    }
+    else{ console.log("O número precisa ser maior que 0!")}
+
+}
+exibirImpares(1)
