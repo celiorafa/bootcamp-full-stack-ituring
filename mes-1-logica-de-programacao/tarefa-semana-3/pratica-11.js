c633a8591c67baa41c1737b64feb7c1fe02a98ee
Celio Rafael <cr2faria@gmail.com>
2022-10-24 12:52:09 -0300

    separar duplas

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-11.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-11.js
@@ -2,26 +2,26 @@ let views = [121, 45, 123, 98, 56, 45, 32, 18, 10, 232, 1392824];
 
 // ordenação criada
 /*function ordenar(views){
-    i=0
-    imax=views.length;
+        i=0
+        imax=views.length;
 
-    ib=0
-while(ib<imax){
-    while(i<imax){ 
+        j=0
+        while(j<imax){
+            while(i<imax){ 
 
 
-        if (views[i] > views[i+1]){
-            a= views[i+1];
-            b= views[i];
-            views[i+1]=b;
-            views[i]=a;
-        } 
-        i++;
-        }
-        ib++;
+                if (views[i] > views[i+1]){
+                    a= views[i+1];
+                    b= views[i];
+                    views[i+1]=b;
+                    views[i]=a;
+                } 
+                i++;
+            }
+        j++;
         i=0;
-    }
-return views
+        }
+    return views
 }
 
 console.log(ordenar(views)) */
---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-12.js
@@ -0,0 +1,23 @@
+let turma = ["Anderson Luz", "Fabio Vechia", "Claudia Lopes", "Rafael Alves", "Maria Mansano", "Jairo Ferreira", "Vitor Ribeiro"]
+
+function divisaoDuplas(turma){
+    const totalTurma = turma.length;
+    const totalGrupos = Math.floor(totalTurma/2);
+    const trio = totalTurma%2;
+    let j=0;
+    let ultimo = ""
+    console.log(totalTurma)
+    console.log(totalGrupos)
+    console.log(trio)
+
+    for (i=0;i<totalGrupos;i++){
+        if (trio==true && i==(totalGrupos-1)){
+            ultimo=`, ${turma[totalTurma-1]}`
+        }
+
+        console.log(`Grupo ${i+1}: ${turma[j]}, ${turma[j+1]}${ultimo}`)
+        j=j+2;
+    }
+}
+
+divisaoDuplas(turma)
