7a01a17c33f95c15db2ea978a04fa2ce2c23982c
Celio Rafael <cr2faria@gmail.com>
2022-10-27 15:43:21 -0300

    usando forEach

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-1.js
@@ -0,0 +1,6 @@
+const listaSeries = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"];
+
+
+listaSeries.forEach(function(item, indice){
+        console.log(`[${indice}] ${item}`)
+})
