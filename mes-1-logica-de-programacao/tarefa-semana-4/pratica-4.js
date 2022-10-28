77c92ede847fd6e12faf4b80fd23406f393b3613
Celio Rafael <cr2faria@gmail.com>
2022-10-28 16:28:06 -0300

    pratica 4 com desafio

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-4.js
@@ -0,0 +1,17 @@
+
+/* -----
+let listaRaiz = numeros.map(function(item){
+    return Math.sqrt(item);
+});
+
+console.log(listaRaiz)
+----*/
+
+// DESAFIO
+
+let numeros = [4, 9, 16, 25, 36, 49];
+
+let listaRaiz = numeros.map(item => (Math.sqrt(item)))
+
+
+console.log(listaRaiz);
