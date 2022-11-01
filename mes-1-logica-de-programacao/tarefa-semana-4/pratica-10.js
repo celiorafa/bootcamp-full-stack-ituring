80040b1e919565ace5ae47011118b76964d42df8
Celio Rafael <cr2faria@gmail.com>
2022-10-31 22:20:54 -0300

    extrato bancário

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-10.js
@@ -0,0 +1,50 @@
+let movimentacao = [{
+    valor: 100.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2021-08-02T07:46:36.611Z"
+  },
+  {
+    valor: 20.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2021-09-17T09:46:36.611Z"
+  },
+  {
+    valor: 30.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-02-24T09:46:36.611Z"
+  },
+  {
+    valor: 10.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2022-03-02T09:46:36.611Z"
+  },
+  {
+    valor: 7.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-04-20T09:46:36.611Z"
+  },
+  {
+    valor: 21.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-03-20T09:46:36.611Z"
+  },
+  {
+    valor: 5.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-06-08T09:46:36.611Z"
+  }]
+
+  function saldoFinal (extrato){
+    let somaTotal = 0
+    extrato.forEach(function(item, i){
+        if (item.movimentacao == "deposito"){
+            somaTotal += item.valor;
+        } else if (item.movimentacao == "retirada"){
+            somaTotal -= item.valor;
+        }   
+
+    })
+    console.log(`O saldo final de sua conta foi positivo no valor de R$ ${somaTotal}`)
+  }
+
+  saldoFinal(movimentacao);
